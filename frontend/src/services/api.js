e3e1165d8abcae1226d45c37779e1ff9e99a1fdd
Cdl Laboratório Santos e Vidal <cdldesenvolvedor@gmail.com>
2026-05-21 14:10:55 -0400

    fix api url production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,30 +1,17 @@
 import axios from 'axios'
 
 const api = axios.create({
-
-    baseURL: 'http://localhost:3000'
-
+  baseURL: import.meta.env.VITE_API_URL
 })
 
-api.interceptors.request.use(
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
 
-    (config) => {
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
 
-        const token = localStorage.getItem(
-            'token'
-        )
-
-        if (token) {
-
-            config.headers.Authorization =
-                `Bearer ${token}`
-
-        }
-
-        return config
-
-    }
-
-)
+  return config
+})
 
 export default api
