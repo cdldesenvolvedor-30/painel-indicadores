1a38ee440a900a67853008256723ef5924cfbdd7
Cdl Laboratório Santos e Vidal <cdldesenvolvedor@gmail.com>
2026-05-22 16:05:28 -0400

    corrigir api frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: 'https://painel-indicadores-production.up.railway.app'
 })
 
 api.interceptors.request.use((config) => {
