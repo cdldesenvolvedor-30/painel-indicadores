205fc04a56d2bbcd8c1e65da9d9f4aff917447c6
cdldesenvolvedor-30 <cdldesenvolvedor@gmail.com>
2026-05-23 12:44:38 -0400

    desativar sync digisac temporariamente

--- server.js
+++ server.js
@@ -1,10 +1,3 @@
-const app = require('./src/app')
 const { iniciarSincronizacaoAutomatica } = require('./src/services/digisacSync.service')
 
-const PORT = process.env.PORT || 3000
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`)
-
-  iniciarSincronizacaoAutomatica()
 })
