1d647818eb550abc84e0cf50dd9c5b3e64c28906
cdldesenvolvedor-30 <cdldesenvolvedor@gmail.com>
2026-05-23 12:49:35 -0400

    corrigir server js

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
-const { iniciarSincronizacaoAutomatica } = require('./src/services/digisacSync.service')
+const app = require('./src/app')
 
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor rodando na porta ${PORT}`)
 })
