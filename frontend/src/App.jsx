430939ae175ee895f7e5f2dab9c35026b278ef1d
cdldesenvolvedor-30 <cdldesenvolvedor@gmail.com>
2026-06-23 10:27:38 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,6 +22,7 @@ import ComparativoMetas from './pages/ComparativoMetas'
 import CRM from './pages/CRM'
 import Integracoes from './pages/Integracoes'
 import AtendimentoCompliance from './pages/AtendimentoCompliance'
+import OX360Financeiro from './pages/OX360Financeiro'
 
 import Sidebar from './components/Sidebar'
 import ProtectedRoute from './components/ProtectedRoute'
@@ -241,6 +242,17 @@ function App() {
     <ProtectedRoute>
       <Layout>
         <AtendimentoCompliance />
+      </Layout>
+    </ProtectedRoute>
+  }
+/>
+        
+            <Route
+  path="/ox360-financeiro"
+  element={
+    <ProtectedRoute>
+      <Layout>
+        <OX360Financeiro />
       </Layout>
     </ProtectedRoute>
   }
